ac4e11e59675650211701fbe0d663843caab517c
Evangelos Karachristos <karachristos.e@outlook.com>
2023-06-24 20:57:27 +0300

    Test for GetAllUserInfo

---
+++ Unit/UsersTests/UserInfoTets.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Users.Repository;
+
+namespace UsersTests
+{
+    public class UserInfoTets
+    {
+        private MockHelper _mockHelper;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockHelper = new MockHelper();
+        }
+
+        [Test]
+        public void GetAllUsers_ReturnsListOfUsers()
+        {
+            // Arrange
+            var expectedUsers = _mockHelper.GetUserInfoList();
+            var userInfoService = new UserInfoService();
+
+            // Act
+            var actualUsers = userInfoService.GetAllUsers();
+
+            // Assert
+            actualUsers.Should().BeEquivalentTo(expectedUsers);
+        }
+    }
+}
+
