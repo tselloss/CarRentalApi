95675446c3d3c47b8020e142f9cbd76a71ae2806
Evangelos Karachristos <karachristos.e@outlook.com>
2023-07-30 17:11:20 +0300

    Change some names values

--- User.Info/Extensions/AuthenticationRequestBody.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace User.Info.Extensions
-{
-    public class AuthenticationRequestBody
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
--- User.Info/Request/UserAuthenticationDto.cs
+++ User.Info/Request/UserAuthenticationDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace CarRentalApi.Requests
 {
-    public class UserDto
+    public class UserAuthenticationDto
     {
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
