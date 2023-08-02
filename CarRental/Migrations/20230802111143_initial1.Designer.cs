670b498ef58d2eb781d6861a848d5fbe47b251ba
Evangelos Karachristos <karachristos.e@outlook.com>
2023-08-02 14:13:17 +0300

    A breakpiont to migrate my database

--- CarRental/Migrations/20230802111143_initial1.Designer.cs
+++ CarRental/Migrations/20230802111143_initial1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CarRentalManagment.PostgresContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CarRental.Migrations
 {
     [DbContext(typeof(PostgresDbContext))]
-    partial class PostgresDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230802111143_initial1")]
+    partial class initial1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CarRental/Migrations/20230802111143_initial1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRental.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "UserInfo");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "UserInfo");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "UserInfo",
+                type: "bytea",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "UserInfo",
+                type: "bytea",
+                nullable: true);
+        }
+    }
+}
